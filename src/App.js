26303299b6f1d267ca7bb91a790df0fb86e0d716
Jung Yunho <a01049048063@gmail.com>
2023-01-06 16:25:30 +0900

    feat: 노마트 코더 1강
    https://www.youtube.com/watch?v=HqIFTMvtVgc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <Fragment key="key">
+        <button type="button" className="button button--success">
+          Hello
+        </button>
+        <button type="button" className="button button--danger">
+          Hello
+        </button>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
